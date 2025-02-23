a69599c6ed8526b2d688b2508d9d2fd161cd27ce
MaikZs <araujo.ncc@gmail.com>
2025-02-23 15:27:01 -0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    images: {
-        unoptimized: true,
-    },
-    basePath: '/primerizos-padre',
-    trailingSlash: true,
-    assetPrefix: '/primerizos-padre/',
-    // Agrega esta configuración para manejar las imágenes
-    webpack: (config) => {
-        config.module.rules.push({
-            test: /\.(png|jpg|jpeg|gif|svg)$/i,
-            type: 'asset/resource',
-        });
-        return config;
-    },
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/primerizos-padre',
+  assetPrefix: '/primerizos-padre/'
 };
 
-export default nextConfig;
+module.exports = nextConfig;
